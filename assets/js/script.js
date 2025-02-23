c94ab6d22b154516b63e9ef85c1a9c6bb39c13d0
Tahazzat-dev <mdtahazzatali820@gmail.com>
2025-02-23 10:24:21 +0600

    dark and light mode created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,114 +1,105 @@
-$(document).ready(function() {
-    // Function to switch languages
-   
+$(document).ready(function () {
+  // Function to switch languages
 
-
-    class LanguageSwitcher {
-        constructor() {
-          this.init();
-        }
-
-        // initialize
-        init() {
-            this.bindEvents();
-        }
-
-        // events
-        bindEvents() {
-            $('#language-selector').on('change',this.changeLanguage.bind(this));
-        }
-
-        // methods
-        changeLanguage(e) {
-            const language = e?.target?.value
-            language && $('html').attr('lang', language);
-            if(language){
-                this.changeElementsTextLan(language);
-                this.changeInputPlaceholderLan(language);
-            }
-        }
-
-        changeElementsTextLan(language){
-            // business logic here.
-        }
-
-        changeInputPlaceholderLan(language){
-            $('.multi-lan').each(function(_i, input){
-                let text;
-                if(language=='bn'){
-                    text = $(input).data('bn');
-                }else{
-                    text = $(input).data('en');
-                }
-
-                $(input).attr('placeholder', text);                
-            })
-        }
-
+  class LanguageSwitcher {
+    constructor() {
+      this.init();
     }
 
-    class themeSwitcher {
-        constructor() {
-          this.init();
-        }
-
-        // initialize
-        init() {
-            this.bindEvents();
-        }
-
-        // events
-        bindEvents() {
-            $('.theme-toggler').on('click',this.toggleTheme.bind(this));
-        }
-
-        // methods
-       toggleTheme(event){
-        const button = event?.currentTarget;
-        $('.theme-toggler').click(function() {
-            let currentVal = $(this).data('val');
-            let newVal = currentVal === 'dark' ? 'light' : 'dark';
-        
-            $(this).data('val', newVal);
-        
-            // Optionally, you can also update the class for visual changes
-            if (newVal === 'dark') {
-              $(this).addClass('dark').removeClass('light');
-            } else {
-              $(this).addClass('light').removeClass('dark');
-            }
-       }
-
+    // initialize
+    init() {
+      this.bindEvents();
     }
 
+    // events
+    bindEvents() {
+      $("#language-selector").on("change", this.changeLanguage.bind(this));
+    }
 
-    const languageSwitcher = new LanguageSwitcher();
+    // methods
+    changeLanguage(e) {
+      const language = e?.target?.value;
+      language && $("html").attr("lang", language);
+      if (language) {
+        this.changeElementsTextLan(language);
+        this.changeInputPlaceholderLan(language);
+      }
+    }
 
+    changeElementsTextLan(language) {
+      // business logic here.
+    }
 
+    changeInputPlaceholderLan(language) {
+      $(".multi-lan").each(function (_i, input) {
+        let text;
+        if (language == "bn") {
+          text = $(input).data("bn");
+        } else {
+          text = $(input).data("en");
+        }
 
-    // slick slider
-    $('.AH_intro-slider').slick({
-        // Basic Settings
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        speed: 400,
-        arrows: false,
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 3000,
-    
-        // Responsive Settings
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-            }
-          }
-        ],
-        fade: true,      
-        // Advanced Options
-        lazyLoad: 'ondemand',
+        $(input).attr("placeholder", text);
       });
+    }
+  }
+
+  class ThemeSwitcher {
+    constructor() {
+      this.init();
+    }
+
+    // initialize
+    init() {
+      this.bindEvents();
+    }
+
+    // events
+    bindEvents() {
+      $(".theme-toggler").on("click", this.toggleTheme.bind(this));
+    }
+
+    // methods
+    toggleTheme(event) {
+      const btn = $(event?.currentTarget);
+      const currentVal = btn.data("val");
+      let newVal = currentVal === "dark" ? "light" : "dark";
+      btn.data("val", newVal);
+
+      // Apply the new theme
+
+      if (newVal === "dark") {
+        $('body').addClass("dark").removeClass("light");
+      } else {
+        $('body').addClass("light").removeClass("dark");
+      }
+    }
+  }
+
+  const languageSwitcher = new LanguageSwitcher();
+  const themeSwitcher = new ThemeSwitcher();
+
+  // slick slider
+  $(".AH_intro-slider").slick({
+    // Basic Settings
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    speed: 400,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+
+    // Responsive Settings
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {},
+      },
+    ],
+    fade: true,
+    // Advanced Options
+    lazyLoad: "ondemand",
   });
-  
+});
